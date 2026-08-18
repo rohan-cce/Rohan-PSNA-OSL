d674f64848e56612f0a0d53d0838ca38690d889d
Rohan J <rohan.j@sap.com>
2026-08-18 09:26:49 +0530

    a+b

--- Example.java
+++ Example.java
@@ -1,8 +1,8 @@
 public class Example{
     static void main() {
         int a=0, b=0;
-        return int add(int a, int b){
-
+        int add(int a, int b){
+            return a+b;
         }
     }
 }
