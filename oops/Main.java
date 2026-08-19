52f0c1cb1a3ccfb64a466b07e202b2d6be13cdca
Rohan J <rohanjcce@gmail.com>
2026-08-19 14:46:23 +0530

    add more constructor

--- oops/Main.java
+++ oops/Main.java
@@ -1,9 +1,26 @@
 class Order {
+    int orderId;
+    String orderStatus;
 
+    public Order(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public Order(int orderId, String orderStatus) {
+        this.orderId = orderId;
+        this.orderStatus = orderStatus;
+    }
 }
 
 public class Main {
     static void main() {
-        Order order = new Order();
+        Order order = new Order(1);
+        System.out.println("Order ID: " + order.orderId);
     }
 }
+
+
+
+
+
+
