1ff665ddbdf25c2c58be88add58e16b11f5a0d61
Rohan J <rohanjcce@gmail.com>
2026-08-19 15:20:59 +0530

    enum

--- oops/Main.java
+++ oops/Main.java
@@ -1,24 +1,36 @@
 class Order {
     public int orderId;
-    public String orderStatus;
+    public OrderStatus orderStatus;
 
     public Order(int orderId) {
         this.orderId = orderId;
     }
 
-    public Order(int orderId, String orderStatus) {
+    public Order(int orderId, OrderStatus orderStatus) {
         this.orderId = orderId;
         this.orderStatus = orderStatus;
     }
 }
 
+enum OrderStatus{
+    PENDING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
+
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
         Order order = new Order(1);
         System.out.println("Order ID: " + order.orderId);
 
-        Order order = new Order(2, "Shipped");
-        System.out.println("Order ID: " + order.orderStatus);
+        Order order1 = new Order(2, OrderStatus.PENDING);
+
+        Order order2 = new Order(2, OrderStatus.PENDING);
+//        Order order2 = new Order(2, OrderStatus.PENDING);
+
+
+        System.out.println("Order status: " + order.orderStatus);
 
 
     }
